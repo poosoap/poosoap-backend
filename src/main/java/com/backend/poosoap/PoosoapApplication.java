3a09f8565885fc3f9e1f2a2451454708ff54a721
LeeJeongGi <apple4rhk@naver.com>
2023-03-13 22:44:08 +0900

    first commit

---
+++ src/main/java/com/backend/poosoap/PoosoapApplication.java
@@ -0,0 +1,13 @@
+package com.backend.poosoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoosoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoosoapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backend/poosoap/PoosoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.poosoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoosoapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
