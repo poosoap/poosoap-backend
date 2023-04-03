d42eb79bd5020d62b0281dc2e9ae94934673a1f6
LeeJeongGi <apple4rhk@naver.com>
2023-04-03 21:40:13 +0900

    refactor : 빈 값 오류 검증 추가

--- src/main/java/com/backend/poosoap/map/dto/req/Location.java
+++ src/main/java/com/backend/poosoap/map/dto/req/Location.java
@@ -1,5 +1,6 @@
 package com.backend.poosoap.map.dto.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Location {
 
+    @NotBlank(message = "위도값은 필수 입니다.")
     private Double latitude;
+
+    @NotBlank(message = "경도값은 필수 입니다.")
     private Double longitude;
 
     public Location(Double latitude, Double longitude) {
