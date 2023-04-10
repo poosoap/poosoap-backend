353fe4ae3792d4f8190b699804dc4eb9ac5fc67d
LeeJeongGi <apple4rhk@naver.com>
2023-04-10 23:10:02 +0900

    refactor : NotBlank -> NotNull(NotBlank는 문자열만 사용 가능 )

--- src/main/java/com/backend/poosoap/map/dto/req/ModifyToiletForm.java
+++ src/main/java/com/backend/poosoap/map/dto/req/ModifyToiletForm.java
@@ -1,6 +1,7 @@
 package com.backend.poosoap.map.dto.req;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,13 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class ModifyToiletForm {
 
-    @NotBlank(message = "변경하려는 화장실 id 값은 필수 입니다.")
+    @NotNull(message = "변경하려는 화장실 id 값은 필수 입니다.")
     private Long id;
 
     @NotBlank(message = "변경하려는 화장실 주소 값은 필수 입니다.")
     private String addr;
 
-    @NotBlank(message = "변경하려는 화장실 위치 값은 필수 입니다.")
+    @NotNull(message = "변경하려는 화장실 위치 값은 필수 입니다.")
     private Location location;
 
     @Builder
